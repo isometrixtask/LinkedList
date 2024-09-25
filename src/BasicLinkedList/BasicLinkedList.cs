6ddfeae3043e675c40adfb12450618fd15bda055
Joseph Mortleman <joseph@mortleman.com>
2024-09-25 13:59:27 +0100

    Change name of head listItem to _head

--- src/BasicLinkedList/BasicLinkedList.cs
+++ src/BasicLinkedList/BasicLinkedList.cs
@@ -2,23 +2,23 @@
 
 public class BasicLinkedList<T>
 {
-    private BasicLinkedListItem<T>? _listItem;
+    private BasicLinkedListItem<T>? _head;
 
     public void Insert(T item) {
 
         var newListItem = new BasicLinkedListItem<T>(item);
-        newListItem.Next = _listItem;
-        _listItem = newListItem;
+        newListItem.Next = _head;
+        _head = newListItem;
     }
 
     public void Delete() {
-        _listItem = _listItem?.Next;
+        _head = _head?.Next;
     }
 
     public string PrintList() {
-        if (_listItem is null) return string.Empty;
+        if (_head is null) return string.Empty;
 
-        var currentListItem = _listItem;
+        var currentListItem = _head;
         var outputString = $"{currentListItem.Value}";
 
         while (currentListItem?.Next != null) {
